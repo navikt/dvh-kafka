d6c8a80c40ff68039c8bb3f63792cb245d964769
Patrick Pedersen <patrick.pedersen@nav.no>
2020-10-23 15:05:45 +0200

    Refactor topic property

--- kafka-konsument/src/main/java/no/nav/dvhSykmKonsument/config/KafkaConfig.java
+++ kafka-konsument/src/main/java/no/nav/dvhSykmKonsument/config/KafkaConfig.java
@@ -1,27 +1,20 @@
 package no.nav.dvhSykmKonsument.config;
 
-import kafka.Kafka;
-import no.nav.dvhSykmKonsument.consumer.IKonsument;
 import no.nav.dvhSykmKonsument.consumer.KafkaKonsument;
 import no.nav.dvhSykmKonsument.model.dvh.Secret;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.*;
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 import static no.nav.dvhSykmKonsument.consumer.IKonsument.*;
@@ -31,8 +24,8 @@ import static org.springframework.util.backoff.FixedBackOff.UNLIMITED_ATTEMPTS;
 @Configuration
 public class KafkaConfig {
     //TODO: Sett til topic du ønsker
-    @Value("${kafka.topic.topic1}")
-    private String topic1;
+    @Value("${kafka.topic}")
+    private String topic;
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaBootstrapServers;
@@ -60,7 +53,7 @@ public class KafkaConfig {
     //TODO: Sett topic du ønsker
     @Bean
     public ContainerProperties containerProperties() {
-        return new ContainerProperties(topic1);
+        return new ContainerProperties(topic);
     }
 
     //TODO: Sett til Konsumenerklassen
