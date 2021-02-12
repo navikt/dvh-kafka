52844e701c44a8a3654ba9ab9f087f273036cd51
Patrick Pedersen <patrick.pedersen@nav.no>
2021-02-12 13:30:47 +0100

    Add a mk_ident service api
    This service is meant to provide some general function commonly used when
    interacting with mk_ident. I'm not yet sure of this is a good idea so
    it will possibly change or be removed in the future.

---
+++ kafka-config/src/main/java/no/nav/dvh/kafka/config/datasource/mkident/MkIdentService.java
@@ -0,0 +1,24 @@
+package no.nav.dvh.kafka.config.datasource.mkident;
+
+import no.nav.dvh.kafka.config.datasource.mkident.model.MkIdent;
+import no.nav.dvh.kafka.config.datasource.mkident.repository.MkIdentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MkIdentService {
+    @Autowired
+    MkIdentRepository mkIdentRepository;
+
+    public MkIdent hentMkIdent(String fodselsnummer) {
+        MkIdent mkIdent = mkIdentRepository.findByPersonIdOff(fodselsnummer);
+        return mkIdent == null ? new MkIdent() : mkIdent;
+    }
+
+    public boolean sperretAdresse(MkIdent mkIdent) {
+        final String kode6 = "SPSF"; // Sperret adresse, strengt fortrolig
+        final String kode7 = "SPFO"; // Sperret adresse, fortrolig
+        String speisalRegisterKode = mkIdent.getSpesialregisterKode();
+        return kode6.equalsIgnoreCase(speisalRegisterKode) || kode7.equalsIgnoreCase(speisalRegisterKode);
+    }
+}
--- kafka-konsument/src/main/java/no/nav/dvh/kafka/konsument/service/KonsumerService.java
+++ kafka-konsument/src/main/java/no/nav/dvh/kafka/konsument/service/KonsumerService.java
@@ -1,39 +1,26 @@
 package no.nav.dvh.kafka.konsument.service;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import no.nav.dvh.kafka.config.datasource.mkident.MkIdentService;
 import no.nav.dvh.kafka.config.datasource.mkident.model.MkIdent;
 import no.nav.dvh.kafka.konsument.model.dvh.DvhModel;
 import no.nav.dvh.kafka.konsument.model.kilde.MottattMelding;
 import no.nav.dvh.kafka.konsument.repository.DvhModelRepository;
-import no.nav.dvh.kafka.config.datasource.mkident.repository.MkIdentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static no.nav.dvh.kafka.config.MappingUtil.mottattMeldingMapper;
+
 
 @Service
 @Transactional
 public class KonsumerService {
 
         @Autowired
-        MkIdentRepository mkIdentRepository;
+        MkIdentService mkIdentService;
 
         @Autowired
         DvhModelRepository dvhModelRepository;
-
-        private ObjectMapper mottattMeldingMapper = new ObjectMapper()
-                .setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
-                .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
-                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-                .setVisibility(PropertyAccessor.CREATOR, JsonAutoDetect.Visibility.ANY)
-                .registerModule(new JavaTimeModule())
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         //TODO: Settes til kilde
         public MottattMelding lagMottattMelding(String mottattMelding) throws Exception {
@@ -44,9 +31,9 @@ public class KonsumerService {
         public DvhModel mottattMeldingTilDvhModelMapper(MottattMelding mottattMelding) throws Exception {
                 DvhModel dvhModel = new DvhModel();
 
-                MkIdent mkIdent = hentMkIdent(mottattMelding.getPersonNr());
+                MkIdent mkIdent = mkIdentService.hentMkIdent(mottattMelding.getPersonNr());
 
-                long akPerson1 = sperretAdresse(mkIdent) ? -1 : mkIdent.getAkPerson1();
+                long akPerson1 = mkIdentService.sperretAdresse(mkIdent) ? -1 : mkIdent.getAkPerson1();
 
                 dvhModel.setAkPerson1(akPerson1);
 
@@ -56,17 +43,5 @@ public class KonsumerService {
         //TODO: Settes til riktig repository
         public void lagreMelding(DvhModel dvhModel) throws Exception {
                 dvhModelRepository.save(dvhModel);
-        }
-
-        private MkIdent hentMkIdent(String fodselsnummer) {
-                MkIdent mkIdent = mkIdentRepository.findByPersonIdOff(fodselsnummer);
-                return mkIdent == null ? new MkIdent() : mkIdent;
-        }
-
-        private boolean sperretAdresse(MkIdent mkIdent) {
-                final String kode6 = "SPSF"; // Sperret adresse, strengt fortrolig
-                final String kode7 = "SPFO"; // Sperret adresse, fortrolig
-                String speisalRegisterKode = mkIdent.getSpesialregisterKode();
-                return kode6.equalsIgnoreCase(speisalRegisterKode) || kode7.equalsIgnoreCase(speisalRegisterKode);
         }
 }
