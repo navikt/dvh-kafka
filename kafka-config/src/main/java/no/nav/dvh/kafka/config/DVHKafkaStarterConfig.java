ce3e5495d9dba68f06a1ae6ee4a8c53135b50934
Patrick Pedersen <patrick.pedersen@nav.no>
2021-02-18 12:47:19 +0100

    Provide default path for fetching secret properties

--- kafka-config/src/main/java/no/nav/dvh/kafka/config/DVHKafkaStarterConfig.java
+++ kafka-config/src/main/java/no/nav/dvh/kafka/config/DVHKafkaStarterConfig.java
@@ -1,9 +1,10 @@
 package no.nav.dvh.kafka.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@Configuration
 @ComponentScan
+@PropertySource(value = "file:${vault.path}", ignoreResourceNotFound = true)
+@PropertySource(value = "file:/var/run/secrets/nais.io/vault/secret.properties", ignoreResourceNotFound = true)
 public class DVHKafkaStarterConfig {
 }
