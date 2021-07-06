31f0e0083e499aa11df7d7ae871c19f51fe27d8b
Patrick Pedersen <patrick.pedersen@nav.no>
2021-07-06 23:14:19 +0200

    Add default configuration for avro consumers

--- kafka-consumer-spring-boot-starter/src/main/java/no/nav/dvh/kafka/consumer/avro/AvroConfig.java
+++ kafka-consumer-spring-boot-starter/src/main/java/no/nav/dvh/kafka/consumer/avro/AvroConfig.java
@@ -1,11 +1,12 @@
 package no.nav.dvh.kafka.consumer.avro;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -17,85 +18,84 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.ContainerStoppingErrorHandler;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.Map;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @EnableKafka
 @Configuration
 class AvroConfig {
 
-    @Value("${kafka.topics}")
+    @Value("${kafka.topics:topic1}")
     private String[] topics;
-
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${KAFKA_BROKERS:localhost:9091}")
     private String kafkaBootstrapServers;
-
-    @Value("${truststore.path}")
+    @Value("${KAFKA_TRUSTSTORE_PATH:}")
     private String truststorePath;
-
-    @Value("${truststore.password}")
+    @Value("${KAFKA_CREDSTORE_PASSWORD:}")
     private String truststorePassword;
-
-    @Value("${serviceuser.username}")
-    private String kafkaUsername;
-
-    @Value("${serviceuser.password}")
-    private String kafkaPassword;
-
-    private String saslJaasConfig;
+    @Value("${KAFKA_KEYSTORE_PATH:}")
+    private String keystorePath;
+    @Value("${KAFKA_CREDSTORE_PASSWORD:}")
+    private String keystorePassword;
+    @Value("${KAFKA_SCHEMA_REGISTRY:http://localhost:8081}")
+    private String schemaRegistryURL;
+    @Value("${KAFKA_SCHEMA_REGISTRY_USER:}")
+    private String schemaRegistryUser;
+    @Value("${KAFKA_SCHEMA_REGISTRY_PASSWORD:}")
+    private String schemaRegistryUserPassword;
 
     @Autowired
     KafkaProperties kafkaProperties;
 
     @Autowired
-    AvroListener konsument;
+    AvroListener consumer;
 
-    public ContainerProperties containerProperties() {
-        ContainerProperties props = new ContainerProperties(topics);
-        props.setAuthorizationExceptionRetryInterval(Duration.ofMinutes(1L));
+    public Map<String, Object> consumerProperties() {
+        var props = kafkaProperties.buildConsumerProperties();
+
+        props.putIfAbsent(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
+
+        if (hasText(truststorePath)) {
+            props.putIfAbsent(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststorePath);
+            props.putIfAbsent(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, truststorePassword);
+        }
+
+        if (hasText(keystorePath)) {
+            props.putIfAbsent(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name);
+            props.putIfAbsent(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, keystorePath);
+            props.putIfAbsent(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, keystorePassword);
+        }
+
+        props.putIfAbsent(KafkaAvroDeserializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+        props.putIfAbsent(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL);
+        props.putIfAbsent(KafkaAvroDeserializerConfig.USER_INFO_CONFIG, "$schemaRegistryUser:$schemaRegistryUserPassword");
+
+        // Override this as spring kafka sets it to StringDeserializer by default
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+
         return props;
-    }
-
-    public MessageListener<String, GenericRecord> listener() {
-        return konsument;
     }
 
     @Bean
     public KafkaMessageListenerContainer<String, GenericRecord> container() {
         KafkaMessageListenerContainer<String, GenericRecord> container =
                 new KafkaMessageListenerContainer<>(consumerFactory(), containerProperties());
-        container.setupMessageListener(listener());
-        container.setErrorHandler(errorHandler());
+        container.setupMessageListener(consumer);
+        container.setErrorHandler(new ContainerStoppingErrorHandler());
         return container;
     }
 
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        kafkaProperties.setBootstrapServers(Arrays.asList(kafkaBootstrapServers));
-        return kafkaProperties.buildConsumerProperties();
-    }
-
-    @Bean
     public ConsumerFactory<String, GenericRecord> consumerFactory() {
-
-        saslJaasConfig = String.format("org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";", kafkaUsername, kafkaPassword);
-        consumerConfigs().put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststorePath);
-        consumerConfigs().put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, truststorePassword);
-        consumerConfigs().put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
-        consumerConfigs().put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        consumerConfigs().put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-
-        ConsumerFactory<String, GenericRecord> consumerFactory =
-                new DefaultKafkaConsumerFactory<>(consumerConfigs());
-        return consumerFactory;
+        return new DefaultKafkaConsumerFactory(consumerProperties());
     }
 
-    @Bean
-    public ContainerStoppingErrorHandler errorHandler() {
-        return new ContainerStoppingErrorHandler();
+    public ContainerProperties containerProperties() {
+        ContainerProperties props = new ContainerProperties(topics);
+        props.setAuthorizationExceptionRetryInterval(Duration.ofMinutes(1L));
+        return props;
     }
 
 }
